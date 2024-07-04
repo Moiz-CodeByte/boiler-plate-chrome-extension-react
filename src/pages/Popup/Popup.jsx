d883e4fad88e3cbd4953793981e10717eecf95b7
MoizCodeByte <moiz8598@gmail.com>
2024-07-04 18:55:38 +0500

    react boilerplate error fixed

---
+++ src/pages/Popup/Popup.jsx
@@ -0,0 +1,14 @@
+import './Popup.css';
+import React, { useState } from 'react';
+
+const Popup = () => {
+  console.log("Popup script running");
+  return (
+    <div className="popup-container">
+    <h1>React Chrome Extension BoilerPlate</h1>
+    <p>Popup Page</p>
+    </div>
+  );
+};
+
+export default Popup;
